15fd90ea284329ca980a209600c5e0742978f325
clickingmouse <clickingmouse@users.noreply.github.com>
2019-12-11 21:24:26 +0800

    pre submission

--- projects/app-portal/src/App.js
+++ projects/app-portal/src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import "./App.css";
 //import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
+import Page from "./pages/Page";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Home />
+      </div>
+      <Switch>
+        <Route path="/products">
+          <Page />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- projects/app-portal/src/pages/Filter.js
+++ projects/app-portal/src/pages/Filter.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 export default function Filter(props) {
+  const [isAvailable, setIsAvailable] = useState(false);
   const handleSubmit = e => {
     e.preventDefault();
-    props.getSugarHandler();
-    console.log("getting sugar");
+    if (isAvailable) {
+      props.getSugarHandler("/?isAvailable=true");
+      console.log("getting isAvailable sugar");
+    } else {
+      props.getSugarHandler();
+      console.log("getting all sugar");
+    }
+  };
+
+  const handleChange = () => {
+    console.log("checked");
+    setIsAvailable(true);
+  };
+  const handleClear = () => {
+    console.log("clearing");
+    setIsAvailable(false);
   };
   return (
     <div>
@@ -16,16 +31,26 @@ export default function Filter(props) {
         }}
         onSubmit={handleSubmit}
       >
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="isAvailable" />
+        <Form.Group controlId="formisAvailableCheckbox">
+          <Form.Check
+            type="checkbox"
+            onChange={handleChange}
+            label="isAvailable"
+            checked={isAvailable}
+          />
         </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="isExpired" disabled />
+        <Form.Group controlId="formisExpiredCheckbox">
+          <Form.Check
+            type="checkbox"
+            onChange={handleChange}
+            label="isExpired"
+            disabled
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Search
         </Button>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" onClick={handleClear}>
           Clear
         </Button>
       </Form>
